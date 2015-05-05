0c4f4515f2f9ac19bb3731bc4759977379f911aa
yamauk <data.googolplex@gmail.com>
2015-05-05 14:32:06 +0900

    modify to use Flask-SocketIO

--- chat.py
+++ chat.py
@@ -1,83 +1,80 @@
 # coding:utf-8
 
+# エンコードをUTFにする
 import sys
 
-# エンコードをUTFにする
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-from flask import Flask, render_template, request, redirect, url_for, session,jsonify
-from geventwebsocket.handler import WebSocketHandler
-from gevent import pywsgi, sleep
+# ロガー設定
+import logging
 
-ws_list = set()
+logging.basicConfig()
 
-def chat_handle(environ, start_response):
-    web_socket = environ['wsgi.websocket']
-    ws_list.add(web_socket)
+import re
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
-    print 'enter:', len(ws_list), environ['REMOTE_ADDR'], environ[
-        'REMOTE_PORT']
 
-    while True:
-        msg = web_socket.receive()
-        if msg is None:
-            break
-
-        remove = set()
-        for s in ws_list:
-            try:
-                s.send(msg)
-                # エラー発生時はメッセージを消す
-            except Exception:
-                remove.add(s)
-
-        for s in remove:
-            ws_list.remove(s)
-
-    print 'exit:', environ['REMOTE_ADDR'], environ['REMOTE_PORT']
-    ws_list.remove(web_socket)
-
-# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'The secret key which ciphers the cookie'
+socketio = SocketIO(app)
 
 
-# ここからウェブアプリケーション用のルーティングを記述
-# index にアクセスしたときの処理
+@socketio.on('join', namespace='/chat')
+def join(data):
+    username = data['username']
+    room = "makalu_chat"
+    join_room(room)
+    emit('join', {'message': 'Hello ' + username, 'username': 'admin'},
+         room=room, namespace='/chat', broadcast=True)
+
+
+@socketio.on('my broadcast event', namespace='/chat')
+def test_message(data):
+    emit('my response',
+         {'username': session['username'], 'message': link_it(data['message'])},
+         broadcast=True)
+
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    emit('my response',
+         {'message': 'Hello ' + session['username'] + ".",
+          'username': 'admin'}, broadcast=True)
+
+
+@socketio.on('disconnect', namespace='/chat')
+def test_disconnect():
+    emit('my response',
+         {'message': 'Bye ' + session['username'] + ".", 'username': 'admin'},
+         broadcast=True)
+    session.clear()
+
+
 @app.route('/')
 def index():
-    title = "ようこそ"
-    # index.html をレンダリングする
-    return render_template('index.html',
-                           title=title)
-
-# /post にアクセスしたときの処理
-@app.route('/login', methods=['GET', 'POST'])
-def post():
-    title = "こんにちは"
-    if request.method == 'POST':
-        # リクエストフォームから「名前」を取得して
-        # セッション変数に格納
-        if len(request.form['name']) != 0:
-            session['name'] = request.form['name']
-            return redirect(url_for('chat'))
-    return redirect(url_for('index'))
+    return render_template('index.html')
 
 
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
-    return render_template('chat.html', name=session['name'])
+    if request.method == 'POST':
+        # 入力値のバリデーションはJSで実行済み
+        session['username'] = request.form['login-input']
+        return render_template('chat.html')
 
 
-# ユーザ情報のjsonを/_jsonに書き込み
-@app.route('/_json')
-def test_json():
-    user_data = {'username':session['name']}
-    return jsonify(user_data)
+def link_it(value):
+    URL_PATTERN = r'([^"]|^)(https?)(://[\w:;/.?%#&=+-]+)'
+    return re.compile(URL_PATTERN).sub(r'\1<a href="\2\3" target="_blank">\2\3</a>', value)
+link_it.is_safe = True
+
 
 if __name__ == '__main__':
-    #app.debug = True  # デバッグモード有効化
-    # app.run()
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+    app.debug = True  # デバッグモード有効化
+    socketio.run(app, port=5555)
+
+    # server = pywsgi.WSGIServer(('0.0.0.0', 5000), app,
+    # handler_class=WebSocketHandler)
+    # server.serve_forever()
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,44 +1,23 @@
-function sendMessage() {
-    //ユーザデータを読み込み，メッセージを表示
-    $.getJSON("/_json", function(data){
-        var message = $(':text[name=message-input]').val();
-        ws.send(message)
-        $("ul").prepend("<li>"+data["username"]+" > "+message+"</li>");
-        //値のクリア
-        $(':text[name=message-input]').val('')
-    });
-}
-
-
-function displayMessage(data) {
-    $("ul").prepend("<li>"+data["username"]+" > "+data['message']+"</li>");
-}
-
-function sendMessage2(ws){
-        var message = $(':text[name=message-input]').val();
-        $.getJSON("/_json", function(user_info){
-        ws.send({'username':user_info['username'],'message':message})
-        });
-}
-function init() {
-
-    ws = new WebSocket('ws://localhost:5000/chat');
-    //メッセージの受信
-    ws.onmessage = function(data) {
-        displayMessage(data)
-    };
-    //メッセージの送信
-    $('button[name=message-button]').click(sendMessage2(ws))
-}
-
 $(document).ready(function(){
-    init()
-    $('button[name=message-button]').click(sendMessage)
+    var socket = io.connect('http://' + document.domain + ':' + location.port+'/chat');
+    socket.on('join',function(data){
+        displayMessage(data)
+    });
+
+    socket.on('my response', function(data) {
+        displayMessage(data)
+    });
+
+    $('#message-button').click(function(event) {
+        if ($(':text[name=message-input]').val()!="" ){
+            socket.emit('my broadcast event', {message: $('input[name=message-input]').val()});
+            $(':text[name=message-input]').val('')
+        }
+        return false;
+    });
 });
 
-function DebugPrint(str)
-{
-    var out = document.getElementById("debug");
-    if (!out) return;
-    out.value += str;
+//チャット画面にデータを表示
+function displayMessage(data) {
+    $("ul").prepend("<li>"+data.username+" > "+data.message+"</li>");
 }
