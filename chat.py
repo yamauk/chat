94d8a9a3df44ddb1e35e3cf9b7aa5830a041e950
yamauk <data.googolplex@gmail.com>
2015-05-07 10:19:06 +0900

    Add functions displaying user name and changing color of message.

--- chat.py
+++ chat.py
@@ -12,13 +12,15 @@ import logging
 logging.basicConfig()
 
 import re
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import redis
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit, join_room
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'The secret key which ciphers the cookie'
 socketio = SocketIO(app)
+red = redis.Redis(host='localhost', port=6379, db=0)
 
 
 @socketio.on('join', namespace='/chat')
@@ -33,21 +35,41 @@ def join(data):
 @socketio.on('my broadcast event', namespace='/chat')
 def test_message(data):
     emit('my response',
-         {'username': session['username'], 'message': link_it(data['message'])},
+         {'username': session['username'], 'color': session['color'],
+          'message': link_it(data['message'])},
          broadcast=True)
 
 
 @socketio.on('connect', namespace='/chat')
 def test_connect():
-    emit('my response',
+    red.hmset(session['username'], session)
+    userlist = red.keys()
+
+    # ユーザ名の順番で色情報を配列に格納
+    colors = []
+    for key in userlist:
+        colors.append(red.hgetall(key)['color'])
+
+    emit('connect_proc',
          {'message': 'Hello ' + session['username'] + ".",
-          'username': 'admin'}, broadcast=True)
+          'username': 'admin', 'color': session['color'],
+          'userlist': userlist, 'colors': colors}, broadcast=True)
 
 
 @socketio.on('disconnect', namespace='/chat')
 def test_disconnect():
-    emit('my response',
-         {'message': 'Bye ' + session['username'] + ".", 'username': 'admin'},
+    red.delete(session['username'])
+    userlist = red.keys()
+
+    # ユーザ名の順番で色情報を配列に格納
+    colors = []
+    for key in userlist:
+        colors.append(red.hgetall(key)['color'])
+
+    emit('disconnect_proc',
+         {'message': 'Bye ' + session['username'] + ".", 'username': 'admin',
+          'color': session['color'],
+          'userlist': userlist},
          broadcast=True)
     session.clear()
 
@@ -62,18 +84,19 @@ def chat():
     if request.method == 'POST':
         # 入力値のバリデーションはJSで実行済み
         session['username'] = request.form['login-input']
+        session['color'] = request.form['login-color']
+        session['ip'] = request.remote_addr
         return render_template('chat.html')
 
 
 def link_it(value):
     URL_PATTERN = r'([^"]|^)(https?)(://[\w:;/.?%#&=+-]+)'
-    return re.compile(URL_PATTERN).sub(r'\1<a href="\2\3" target="_blank">\2\3</a>', value)
+    return re.compile(URL_PATTERN).sub(
+        r'\1<a href="\2\3" target="_blank">\2\3</a>', value)
+
+
 link_it.is_safe = True
 
-
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0')
-
-    # server = pywsgi.WSGIServer(('0.0.0.0', 5000), app,
-    # handler_class=WebSocketHandler)
-    # server.serve_forever()
+    app.debug = True  # デバッグモード有効化
+    socketio.run(app, port=5555)
--- static/js/chat.js
+++ static/js/chat.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
         displayMessage(data)
     });
 
+    socket.on('connect_proc',function(data){
+        displayMessage(data)
+        displayMembers(data)
+    });
+    socket.on('disconnect_proc',function(data){
+        displayMessage(data)
+        displayMembers(data)
+    });
+
     $('#message-button').click(function(event) {
         if ($(':text[name=message-input]').val()!="" ){
             socket.emit('my broadcast event', {message: $('input[name=message-input]').val()});
@@ -19,5 +28,12 @@ $(document).ready(function(){
 
 //チャット画面にデータを表示
 function displayMessage(data) {
-    $("ul").prepend("<li>"+data.username+" > "+data.message+"</li>");
+    var str="<li class=\"message\">"+"<font color=\""+data.color+"\">"+data.username+"</font> > <font color=\""+data.color+"\">"+data.message+"</font></li>"
+    $("#message").prepend(str);
+}
+function displayMembers(data) {
+    $("#member li").remove()
+    for (var i=0;i<data.userlist.length;i++){
+        $("#member").prepend("<li><font color=\""+data.colors[i]+"\">"+data.userlist[i]+"</font></li>");
+    }
 }
